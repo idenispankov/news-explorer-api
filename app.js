c77b2313081927daee18c183e33a54b3c30b2947
Denis Pankov <idenispankov@gmail.com>
2021-04-29 15:34:29 -0700

    user schema created

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const mongoose = require("mongoose");
 const app = express();
 const { PORT = 3000 } = process.env;
 
+mongoose.connect("mongodb://localhost:27017/newsdb", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
 app.listen(PORT, () => {
   console.log(`Server run on port ${PORT}`);
 });
